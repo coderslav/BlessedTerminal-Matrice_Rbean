3439c6b546f09725e6700270f604e8563294ca1a
rkomin <kominslav@gmail.com>
2021-11-30 17:40:39 +0100

    Step 4 and beginning of Step 5

--- cursed_terminal.js
+++ cursed_terminal.js
@@ -1,4 +1,6 @@
 const blessed = require('blessed');
+const axios = require('axios');
+const API_KEY = require('./key.js');
 
 function timeHandler() {
     let timeObject = new Date();
@@ -76,7 +78,35 @@ let weatherBox = blessed.box({
     width: '40%',
     height: '50%',
     align: 'center',
-    content: `\n\nMemory`,
+    content: '',
+    tags: true,
+    border: {
+        type: 'line',
+    },
+    style: {
+        fg: 'white',
+        bg: 'black',
+        border: {
+            fg: '#f0f0f0',
+        },
+    },
+});
+let weatherImage = blessed.image({
+    parent: weatherBox,
+    top: 0,
+    left: '25%',
+    width: '500',
+    height: '500',
+    type: 'ansi',
+    file: '',
+});
+
+let exchangeRate = blessed.box({
+    left: '60%',
+    width: '20%',
+    height: '50%',
+    content: `\n\n\nBTC\n\n\n\n\n\nUSD\n57,687.04`,
+    align: 'center',
     tags: true,
     border: {
         type: 'line',
@@ -93,11 +123,15 @@ let weatherBox = blessed.box({
 screen.append(box);
 screen.append(memoryBox);
 screen.append(weatherBox);
+screen.append(exchangeRate);
 memoryBox.append(memoryPercentBox);
+weatherBox.append(weatherImage);
+
+screen.render();
 
 setInterval(() => {
     box.setContent(`\n\n\n\n\n${timeHandler().time}\n\n\n\n\n${timeHandler().date}`);
-    screen.render(box);
+    screen.render();
 }, 1000);
 
 setInterval(() => {
@@ -109,3 +143,21 @@ setInterval(() => {
     }
     memoryPercentBox.setContent(`${memoryUsageUpdatePercent}%`);
 }, 5000);
+
+axios
+    .get(`https://api.worldweatheronline.com/premium/v1/weather.ashx?key=${API_KEY}&q=Paris&format=json&date=today`)
+    .then((data) => {
+        weatherBox.setContent('\n\n\n\n\n\n\n\n\n\nParis ' + data.data.data.current_condition[0].temp_C + '°C\n\n\n\n');
+        weatherImage.setImage(data.data.data.current_condition[0].weatherIconUrl[0].value);
+    })
+    .catch((err) => console.log(err));
+
+setInterval(() => {
+    axios
+        .get(`https://api.worldweatheronline.com/premium/v1/weather.ashx?key=${API_KEY}&q=Paris&format=json&date=today`)
+        .then((data) => {
+            weatherBox.setContent('\n\n\n\n\n\n\n\n\n\nParis ' + data.data.data.current_condition[0].temp_C + '°C');
+            weatherImage.setImage(data.data.data.current_condition[0].weatherIconUrl[0].value);
+        })
+        .catch((err) => console.log(err));
+}, 15000);
---
+++ key.js
@@ -0,0 +1,3 @@
+const API_KEY = 'baa2a862c0814524ae6144518213011';
+
+module.exports = API_KEY;
