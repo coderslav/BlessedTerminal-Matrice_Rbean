74161ed77edf3e0e65a7405df50ae0fdd336a284
rkomin <kominslav@gmail.com>
2021-12-02 17:05:08 +0100

    Some Fixes + 2 Bonuses done

--- cursed_terminal.js
+++ cursed_terminal.js
@@ -3,9 +3,9 @@ const axios = require('axios');
 const WEATHER_API_KEY = require('./weather_key.js');
 const CRYPTOCURRENCY_API_KEY = require('./cryptocurrency_key.js');
 
-function timeHandler() {
+function timeHandler(format) {
     let timeObject = new Date();
-    return { time: timeObject.toLocaleString('en-GB', { hour: '2-digit', minute: '2-digit', second: '2-digit' }), date: timeObject.toLocaleString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric' }) };
+    return { time: timeObject.toLocaleString(format, { hour: '2-digit', minute: '2-digit', second: '2-digit' }), date: timeObject.toLocaleString(format, { day: '2-digit', month: '2-digit', year: 'numeric' }) };
 }
 function memoryUsageHandler() {
     let memoryData = process.memoryUsage();
@@ -25,7 +25,7 @@ screen.key(['escape', 'q', 'C-c'], function (ch, key) {
 let box = blessed.box({
     width: '20%',
     height: '50%',
-    content: `\n\n\n\n\n${timeHandler().time}\n\n\n\n\n${timeHandler().date}`,
+    content: `\n\n\n\n\n${timeHandler('en-GB').time}\n\n\n\n\n${timeHandler('en-GB').date}`,
     align: 'center',
     tags: true,
     border: {
@@ -219,48 +219,132 @@ let cancelButton = blessed.box({
         },
     },
 });
+let inputField = blessed.textbox({
+    top: '70%',
+    left: '36.5%',
+    width: '27%',
+    height: '10%',
+    value: 'Choose a City',
+    align: 'center',
+    mouse: 'enabled',
+    inputOnFocus: true,
+    style: {
+        fg: 'white',
+        bg: 'blue',
+        hover: {
+            fg: 'black',
+            bg: 'white',
+        },
+    },
+});
+let changeTimeFormatButton = blessed.button({
+    top: '70%',
+    left: '16%',
+    width: '68%',
+    height: '10%',
+    content: 'US Date/Time Format',
+    mouse: 'enabled',
+    style: {
+        fg: 'white',
+        bg: 'blue',
+        hover: {
+            fg: 'black',
+            bg: 'white',
+        },
+    },
+});
 
 submitButton.addListener('click', () => {
     if (radioButtonUSD.value || radioButtonGBP.value || radioButtonEUR.value) {
+        let ticker;
         if (radioButtonUSD.value) {
-            axios
-                .get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=${CRYPTOCURRENCY_API_KEY}&symbol=BTC&convert=USD`)
-                .then((data) => {
-                    let formattedBTCprice = parseFloat(data.data.data.BTC.quote.USD.price.toFixed(4)).toLocaleString('en-FR', { minimumFractionDigits: 4 });
-                    exchangeCurrency.setContent(`USD\n${formattedBTCprice}`);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
+            ticker = 'USD';
+        } else if (radioButtonGBP.value) {
+            ticker = 'GBP';
+        } else {
+            ticker = 'EUR';
         }
-        if (radioButtonGBP.value) {
-            axios
-                .get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=${CRYPTOCURRENCY_API_KEY}&symbol=BTC&convert=GBP`)
-                .then((data) => {
-                    let formattedBTCprice = parseFloat(data.data.data.BTC.quote.GBP.price.toFixed(4)).toLocaleString('en-FR', { minimumFractionDigits: 4 });
-                    exchangeCurrency.setContent(`GBP\n${formattedBTCprice}`);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        }
-        if (radioButtonEUR.value) {
-            axios
-                .get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=${CRYPTOCURRENCY_API_KEY}&symbol=BTC&convert=EUR`)
-                .then((data) => {
-                    let formattedBTCprice = parseFloat(data.data.data.BTC.quote.EUR.price.toFixed(4)).toLocaleString('en-FR', { minimumFractionDigits: 4 });
-                    exchangeCurrency.setContent(`EUR\n${formattedBTCprice}`);
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
-        }
+        axios
+            .get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=${CRYPTOCURRENCY_API_KEY}&symbol=BTC&convert=${ticker}`)
+            .then((data) => {
+                let formattedBTCprice;
+                if (ticker === 'USD') {
+                    formattedBTCprice = parseFloat(data.data.data.BTC.quote.USD.price.toFixed(4)).toLocaleString('en-FR', { minimumFractionDigits: 4 });
+                } else if (ticker === 'EUR') {
+                    formattedBTCprice = parseFloat(data.data.data.BTC.quote.EUR.price.toFixed(4)).toLocaleString('en-FR', { minimumFractionDigits: 4 });
+                } else {
+                    formattedBTCprice = parseFloat(data.data.data.BTC.quote.GBP.price.toFixed(4)).toLocaleString('en-FR', { minimumFractionDigits: 4 });
+                }
+                exchangeCurrency.setContent(`${ticker}\n${formattedBTCprice}`);
+                screen.render();
+            })
+            .catch((err) => {
+                console.log(err);
+            });
         screen.remove(exchangeCurrencyChoice);
     }
 });
 
 cancelButton.addListener('click', () => {
     screen.remove(exchangeCurrencyChoice);
+    screen.render();
+});
+
+inputField.once('focus', () => {
+    inputField.clearValue();
+    inputField.style.fg = 'black';
+    inputField.style.bg = 'white';
+    screen.render();
+});
+inputField.addListener('submit', () => {
+    axios
+        .get(`https://api.worldweatheronline.com/premium/v1/weather.ashx?key=${WEATHER_API_KEY}&q=${inputField.value}&format=json&date=today`)
+        .then((data) => {
+            if (data.data.data.error) {
+                inputField.clearValue();
+                inputField.style.fg = 'red';
+                inputField.style.hover.fg = 'red';
+                inputField.setValue('Not found :(');
+                inputField.once('focus', () => {
+                    inputField.clearValue();
+                    inputField.style.fg = 'black';
+                    inputField.style.bg = 'white';
+                    screen.render();
+                });
+                screen.render();
+            } else {
+                inputField.clearValue();
+                inputField.style.fg = 'white';
+                inputField.style.bg = 'blue';
+                inputField.style.hover.fg = 'black';
+                inputField.setValue('Choose a City');
+                inputField.once('focus', () => {
+                    inputField.clearValue();
+                    inputField.style.fg = 'black';
+                    inputField.style.bg = 'white';
+                    screen.render();
+                });
+                weatherBox.setContent(`\n\n\n\n\n\n\n\n\n\n${data.data.data.request[0].query}: ` + data.data.data.current_condition[0].temp_C + '°C');
+                weatherImage.setImage(data.data.data.current_condition[0].weatherIconUrl[0].value);
+                screen.render();
+            }
+        })
+        .catch((err) => console.log(err));
+});
+
+changeTimeFormatButton.addListener('press', () => {
+    let format;
+    if (box.content.includes('PM') || box.content.includes('AM')) {
+        format = 'en-GB';
+        box.setContent(`\n\n\n\n\n${timeHandler(format).time}\n\n\n\n\n${timeHandler(format).date}`);
+        changeTimeFormatButton.setContent('US Date/Time Format');
+        screen.render();
+    } else {
+        format = 'en-US';
+        box.setContent(`\n\n\n\n\n${timeHandler(format).time}\n\n\n\n\n${timeHandler(format).date}`);
+        changeTimeFormatButton.setContent('EU Date/Time Format');
+        screen.render();
+    }
 });
 
 screen.append(box);
@@ -270,6 +354,8 @@ screen.append(exchangeRate);
 screen.append(exchangeCurrencyChoice);
 memoryBox.append(memoryPercentBox);
 weatherBox.append(weatherImage);
+weatherBox.append(inputField);
+box.append(changeTimeFormatButton);
 exchangeRate.append(exchangeCurrency);
 exchangeCurrencyChoice.append(radioButtonUSD);
 exchangeCurrencyChoice.append(radioButtonEUR);
@@ -282,7 +368,7 @@ screen.render();
 axios
     .get(`https://api.worldweatheronline.com/premium/v1/weather.ashx?key=${WEATHER_API_KEY}&q=Paris&format=json&date=today`)
     .then((data) => {
-        weatherBox.setContent('\n\n\n\n\n\n\n\n\n\nParis ' + data.data.data.current_condition[0].temp_C + '°C\n\n\n\n');
+        weatherBox.setContent(`\n\n\n\n\n\n\n\n\n\n${data.data.data.request[0].query}: ` + data.data.data.current_condition[0].temp_C + '°C');
         weatherImage.setImage(data.data.data.current_condition[0].weatherIconUrl[0].value);
     })
     .catch((err) => console.log(err));
@@ -298,8 +384,16 @@ axios
     });
 
 setInterval(() => {
-    box.setContent(`\n\n\n\n\n${timeHandler().time}\n\n\n\n\n${timeHandler().date}`);
-    screen.render();
+    let format;
+    if (box.content.includes('PM') || box.content.includes('AM')) {
+        format = 'en-US';
+        box.setContent(`\n\n\n\n\n${timeHandler(format).time}\n\n\n\n\n${timeHandler(format).date}`);
+        screen.render();
+    } else {
+        format = 'en-GB';
+        box.setContent(`\n\n\n\n\n${timeHandler(format).time}\n\n\n\n\n${timeHandler(format).date}`);
+        screen.render();
+    }
 }, 1000);
 
 setInterval(() => {
@@ -313,47 +407,40 @@ setInterval(() => {
 }, 1000);
 
 setInterval(() => {
+    let weatherContentList = weatherBox.content.split(',');
+    let weatherContentCity = weatherContentList[0].replaceAll('\n', '');
     axios
-        .get(`https://api.worldweatheronline.com/premium/v1/weather.ashx?key=${API_KEY}&q=Paris&format=json&date=today`)
+        .get(`https://api.worldweatheronline.com/premium/v1/weather.ashx?key=${WEATHER_API_KEY}&q=${weatherContentCity}&format=json&date=today`)
         .then((data) => {
-            weatherBox.setContent('\n\n\n\n\n\n\n\n\n\nParis ' + data.data.data.current_condition[0].temp_C + '°C');
+            weatherBox.setContent(`\n\n\n\n\n\n\n\n\n\n${data.data.data.request[0].query}: ` + data.data.data.current_condition[0].temp_C + '°C');
             weatherImage.setImage(data.data.data.current_condition[0].weatherIconUrl[0].value);
         })
         .catch((err) => console.log(err));
 }, 5 * 60000);
 
 setInterval(() => {
+    let ticker;
     if (exchangeCurrency.content.includes('USD')) {
-        axios
-            .get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=${CRYPTOCURRENCY_API_KEY}&symbol=BTC&convert=USD`)
-            .then((data) => {
-                let formattedBTCprice = parseFloat(data.data.data.BTC.quote.USD.price.toFixed(4)).toLocaleString('en-FR', { minimumFractionDigits: 4 });
-                exchangeCurrency.setContent(`USD\n${formattedBTCprice}`);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
+        ticker = 'USD';
+    } else if (exchangeCurrency.content.includes('EUR')) {
+        ticker = 'EUR';
+    } else if (exchangeCurrency.content.includes('GBP')) {
+        ticker = 'GBP';
     }
-    if (exchangeCurrency.content.includes('EUR')) {
-        axios
-            .get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=${CRYPTOCURRENCY_API_KEY}&symbol=BTC&convert=EUR`)
-            .then((data) => {
-                let formattedBTCprice = parseFloat(data.data.data.BTC.quote.EUR.price.toFixed(4)).toLocaleString('en-FR', { minimumFractionDigits: 4 });
-                exchangeCurrency.setContent(`EUR\n${formattedBTCprice}`);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }
-    if (exchangeCurrency.content.includes('GBP')) {
-        axios
-            .get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=${CRYPTOCURRENCY_API_KEY}&symbol=BTC&convert=GBP`)
-            .then((data) => {
-                let formattedBTCprice = parseFloat(data.data.data.BTC.quote.GBP.price.toFixed(4)).toLocaleString('en-FR', { minimumFractionDigits: 4 });
-                exchangeCurrency.setContent(`GBP\n${formattedBTCprice}`);
-            })
-            .catch((err) => {
-                console.log(err);
-            });
-    }
-}, 10 * 6000);
+    axios
+        .get(`https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=${CRYPTOCURRENCY_API_KEY}&symbol=BTC&convert=${ticker}`)
+        .then((data) => {
+            let formattedBTCprice;
+            if (ticker === 'USD') {
+                formattedBTCprice = parseFloat(data.data.data.BTC.quote.USD.price.toFixed(4)).toLocaleString('en-FR', { minimumFractionDigits: 4 });
+            } else if (ticker === 'EUR') {
+                formattedBTCprice = parseFloat(data.data.data.BTC.quote.EUR.price.toFixed(4)).toLocaleString('en-FR', { minimumFractionDigits: 4 });
+            } else if (ticker === 'GBP') {
+                formattedBTCprice = parseFloat(data.data.data.BTC.quote.GBP.price.toFixed(4)).toLocaleString('en-FR', { minimumFractionDigits: 4 });
+            }
+            exchangeCurrency.setContent(`${ticker}\n${formattedBTCprice}`);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}, 5 * 6000);
